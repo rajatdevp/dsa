9698bba185098fb0b92bd626e70983cff93ba80e
Rajat Kumar <rajatsrajesh@gmail.com>
2026-08-25 00:49:10 +0530

    Find the largest element. //recursiveMax

--- src/main/java/in/rajat/dsa/l1/MaxElement.java
+++ src/main/java/in/rajat/dsa/l1/MaxElement.java
@@ -5,6 +5,7 @@ public class MaxElement {
     public static void main(String[] args) {
         int[] arr = {10, 5, 20, 8, 20, 15};
         System.out.println(max(arr));
+        System.out.println(recursiveMax(arr,0));
     }
 
 
@@ -16,4 +17,13 @@ public class MaxElement {
         }
         return max;
     }
+    public static int recursiveMax(int[] arr, int index) {
+        if (index == arr.length - 1) {
+            return arr[index];
+        }
+        int maxOfRest = recursiveMax(arr, index + 1);
+
+        return Math.max(arr[index], maxOfRest);
+    }
+
 }
