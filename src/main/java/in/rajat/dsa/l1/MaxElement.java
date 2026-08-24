adab22f7b3c25ff7d5961d706acfc07de3b2fbbc
Rajat Kumar <rajatsrajesh@gmail.com>
2026-08-25 01:01:48 +0530

    recursiveTable

--- src/main/java/in/rajat/dsa/l1/MaxElement.java
+++ src/main/java/in/rajat/dsa/l1/MaxElement.java
@@ -3,17 +3,19 @@ package in.rajat.dsa.l1;
 public class MaxElement {
 
     public static void main(String[] args) {
-        int[] arr = {10, 5, 20, 8, 20, 15};
+        int[] arr = {10, 5, 20, 8, 20, 15, 38};
         System.out.println(max(arr));
         System.out.println(recursiveMax(arr,0));
+        recursiveTable(1,2);
+
     }
 
 
     public static int max(int[] arr) {
         int max= arr[0];
         for (int j : arr) {
-            //max = Math.max(max, j);
-            max = (j > max) ? j : max;
+            max = Math.max(max, j);
+            //max = (j > max) ? j : max;
         }
         return max;
     }
@@ -24,6 +26,15 @@ public class MaxElement {
         int maxOfRest = recursiveMax(arr, index + 1);
 
         return Math.max(arr[index], maxOfRest);
+    }
+
+    public static void recursiveTable(int index, int n){
+        if (index == 10){
+            System.err.println(n*index);
+            return;
+        }
+        System.err.println(n*index);
+        recursiveTable(index+1, n);
     }
 
 }
