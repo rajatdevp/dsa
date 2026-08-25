0848832f9fb4686ba72a909d4a3140b4ee9248bd
Rajat Kumar <rajatsrajesh@gmail.com>
2026-08-25 22:06:58 +0530

    Find the second-largest element.

---
+++ src/main/java/in/rajat/dsa/l1/IsStored.java
@@ -0,0 +1,22 @@
+package in.rajat.dsa.l1;
+
+public class IsStored {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 6, 3, 4, 5};
+        System.out.println(isSorted(arr));
+    }
+
+    private static boolean isSorted(int arr[]) {
+
+        if (arr.length == 1) return true;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
